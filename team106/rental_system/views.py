f214e96f564e5826a3ccc8f1ebdd4807b6163677
Noor-30412 <2100030412@kluniversity.in>
2023-03-23 16:51:41 +0530

    Made login and signup work and nav bar with home and booking

--- team106/rental_system/views.py
+++ team106/rental_system/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def booking(request):
+    return render(request, 'booking.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def profile(request):
+    rentals = Rental.objects.filter(user=request.user)
+    bookings = Booking.objects.filter(user=request.user)
+    context = {
+        'user': request.user,
+        'rentals': rentals,
+        'bookings': bookings,
+    }
+    return render(request, 'profile.html', context)
--- team106/team106/settings.py
+++ team106/team106/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ag8ooz=!b_rg5md8n-fl%_q5q0+e!3x@i2y4j0qqu^vrj2p)*7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rental_system',
 ]
 
 MIDDLEWARE = [
@@ -112,7 +113,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (css, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
